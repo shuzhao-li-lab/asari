916030e7b76df17c635fe0d88bfa1eb486070ec1
Joshua Mitchell <joshua.mitchell@jax.org>
2023-06-29 16:48:06 -0400

    a reticulate-friendly __init__

--- asari/__init__.py
+++ asari/__init__.py
@@ -1,25 +1,17 @@
 __version__ = "1.11.6"
 
-import sys
-import importlib
-import pkgutil
-
-def import_submodules(package_name):
-    '''
-    Import all submodules of a module, recursively
-
-    Parameters
-    ----------
-    package_name : str
-        the name of a package
-
-    Outputs
-    -------
-    dict[types.ModuleType]
-    '''
-    package = sys.modules[package_name]
-    return {
-        name: importlib.import_module(package_name + '.' + name)
-        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__)
-    }
-__all__ = import_submodules(__name__).keys()
+from . import analyze
+from . import annotate_user_table
+from . import chromatograms
+from . import command_line
+from . import constructors
+from . import dashboard
+from . import default_parameters
+from . import experiment
+from . import json_encoder
+from . import main
+from . import mass_functions
+from . import peaks
+from . import qc
+from . import samples
+from . import workflow
