c5a71eee92a582c9b88faaacff7bf51a99380636
Joshua Mitchell <joshua.mitchell@jax.org>
2023-08-01 12:48:26 -0400

    faster, more parallelized join, prototype for future alignment algorithm

--- asari/test_join.py
+++ asari/test_join.py
@@ -1,119 +1,197 @@
+import os 
+
 import pandas as pd
-import sys
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy as np
 import statsmodels.api as sm
+import multiprocessing as mp
 
-similar_chromatography = False
+def find_anchors(t, similar_chromatography=False, rt_similarity=0.05, mz_ppm=5):
+    anchors = []
+    t['mz_min'] = t['mz'] - t['mz'] / 1e6 * mz_ppm
+    t['mz_max'] = t['mz'] + t['mz'] / 1e6 * mz_ppm
+    t['rt_min'] = t['rtime'] - rt_similarity * t['rtime']
+    t['rt_max'] = t['rtime'] + rt_similarity * t['rtime']
+    for mz_min, mz_max, rt_min, rt_max in zip(t['mz_min'], t['mz_max'], t['rt_min'], t['rt_max']):
+        t_sel = t[t['mz'].between(mz_min, mz_max)]
+        if similar_chromatography:
+            t_sel = t_sel[t_sel['rtime'].between(rt_min, rt_max)]
+        anchors.append(t_sel.shape[0]==1)
+    t['anchor'] = anchors
+    return t
 
-def join_ftables(t1, t2, similar_chromatography=False, dry_run=False, debug=False, rt_similarity=0.05):
+def join_ftables(ftables, similar_chromatography=False, rt_similarity=0.05, mz_ppm=5):
+    workers = mp.Pool(mp.cpu_count())
+    mp.freeze_support()
+
+    iter = 0
+    index = len(ftables)
+    ftable_objects = []
+    for ft in ftables:
+        try:
+            ftable_objects.append(pd.read_csv(ft, sep="\t"))
+        except:
+            pass
+
+    ftables = {i: t for i, t in enumerate(workers.starmap(find_anchors, [(x, similar_chromatography, rt_similarity, mz_ppm) for x in ftable_objects]))}
+    fail_list = []
+    scores = {}
+    while len(list(ftables.keys())) > 1:
+        iter += 1
+        list_keys = list(ftables.keys())
+        to_compute = []
+        for i, k1 in enumerate(list_keys):
+            for k2 in list_keys[i + 1:]:
+                if (k1, k2) not in scores:
+                    to_compute.append((k1, k2))
+
+        new_scores = workers.starmap(map_anchors, [(ftables[x[0]], ftables[x[1]], similar_chromatography) for x in to_compute])  
+        for x, anchor_mapping in zip(to_compute, new_scores):
+            scores[(x[0],x[1])] = (len(anchor_mapping)/min(ftables[x[0]].shape[0], ftables[x[1]].shape[0]), anchor_mapping)
+
+        best_pair = [0, None, None, None]
+        for (key1, key2), (score, mapping) in scores.items():
+            if score > best_pair[0] and (key1, key2) not in fail_list:
+                best_pair = [score, mapping, key1, key2]
+        if best_pair[1] is None:
+            for id, table in ftables.items():
+                table.sort_values(by=['mz', 'rtime'], inplace=True)
+                table.to_csv("joined_table_" + str(id) + ".tsv", sep="\t")
+            return ftables
+
+        joined_table = join_table_pair(ftables[best_pair[2]], ftables[best_pair[3]], best_pair[1])
+        del scores[(best_pair[2], best_pair[3])]
+        if joined_table[0]:
+            ftables[index + iter] = joined_table[1]
+            to_del = []
+            for k1, k2 in scores.keys():
+                if k1 in (best_pair[2], best_pair[3]) or k2 in (best_pair[2], best_pair[3]):
+                    to_del.append((k1, k2))
+            for td in to_del:
+                del scores[td]
+            del ftables[best_pair[2]]
+            del ftables[best_pair[3]]
+        else:
+            fail_list.append((best_pair[1], best_pair[2]))
+    print("OUT")
+    for id, table in ftables.items():
+        table.sort_values(by=['mz', 'rtime'], inplace=True)
+        table.to_csv("joined_table_" + str(id) + ".tsv", sep="\t")
+    list(ftables.values())[0].to_csv("final_paired.tsv", sep="\t")
+
+def anchor_mapping_wrapper(input):
+    return map_anchors(input['t1'], input['t2'], input['similar_chromatography'])
+
+def map_anchors(t1, t2, similar_chromatography):
+    t1_x = t1[t1["anchor"] == True]
+    t2_x = t2[t2["anchor"] == True]
+    common_ion_rts = []
+    for mz1, mz1_min, mz1_max, rt1, rt1_min, rt1_max in zip(t1_x['mz'], t1_x['mz_min'], t1_x['mz_max'], t1_x['rtime'], t1_x['rt_min'], t1_x['rt_max']):
+        if similar_chromatography:
+            t2_x_sel = t2_x[(t2_x['rtime'].between(rt1_min, rt1_max)) & (t2_x['mz'].between(mz1_min, mz1_max))]
+        else:
+            t2_x_sel = t2_x[t2_x['mz'].between(mz1_min, mz1_max)]
+        if t2_x_sel.shape[0] == 1:
+            common_ion_rts.append((rt1, t2_x_sel['rtime'].values[0], mz1, t2_x_sel['mz'].values[0]))
+    return np.array(common_ion_rts)
+
+def find_best_frac_lowess(t1, anchor_mapping, it=3):
+    lowess = sm.nonparametric.lowess
+    frac = 1.0
+    last_frac = None
+    last_error = np.inf
+    while frac > 0.2:
+        found_solution = False
+        frac -= 0.05
+        predicted = lowess(anchor_mapping[:,1], anchor_mapping[:,0], it=it, xvals=anchor_mapping[:,0], frac=frac)
+        new_error = np.sum(anchor_mapping[:,1] - lowess(anchor_mapping[:,1], anchor_mapping[:,0], it=it, xvals=anchor_mapping[:,0], frac=frac))
+        if abs(new_error) < last_error and np.count_nonzero(np.isnan(predicted)) == 0:
+            full_range = lowess(anchor_mapping[:,1], anchor_mapping[:,0], it=it, xvals=range(int(min((t1['rtime']))), int(max(t1['rtime']))), frac=frac)
+            if np.count_nonzero(np.isnan(full_range)) == 0:
+                found_solution = True
+                last_frac = frac
+        if not found_solution:
+            break
+    return last_frac
+
+def join_table_pair(t1, t2, anchor_mapping):
     def __extract_row(row, columns):
         return {column: row[column] for column in columns}
+    best_frac = find_best_frac_lowess(t1, anchor_mapping)
     lowess = sm.nonparametric.lowess
 
-    common_ion_rts = []
-    for mz1, rt1 in zip(t1['mz'], t1['rtime']):
-        matches_in_t1 = 0
-        for mz1_a, rt1_a in zip(t1['mz'], t1['rtime']):
-            if mz1 - mz1/1e6 * 5 < mz1_a < mz1 + mz1/1e6 * 5:
-                if not similar_chromatography or (abs(rt1 - rt1_a) < rt_similarity * rt1 and similar_chromatography):
-                    matches_in_t1 += 1
-        if matches_in_t1 == 1:
-            matches_in_t2 = 0
-            these_commmon_ion_rts = []
-            for mz2, rt2 in zip(t2['mz'], t2['rtime']):
-                if mz1 - mz1/1e6 * 5 < mz2 < mz1 + mz1/1e6 *5:
-                    if not similar_chromatography or (abs(rt1 - rt2) < rt_similarity * rt1 and similar_chromatography):
-                        matches_in_t2 += 1
-                        these_commmon_ion_rts.append((rt1, rt2, mz1, mz2))
-            if len(these_commmon_ion_rts) == 1:
-                common_ion_rts.append(these_commmon_ion_rts[0])
-    if dry_run:
-        return len(common_ion_rts)
+    if best_frac is None:
+        return (False, t1, t2)
+    else:
+        t1['pred_t2_rtime'] = lowess(anchor_mapping[:,1], anchor_mapping[:,0], it=3, xvals=t1['rtime'], frac=best_frac)
 
-    common_ion_rts = np.array(common_ion_rts)
-    last_predicted = lowess(common_ion_rts[:,1], common_ion_rts[:,0], it=50, xvals=common_ion_rts[:,0], frac=1)
-    last_error = abs(np.sum(common_ion_rts[:,1] - last_predicted))
-    last_frac = 1.0
-    frac = 1.0
-    while frac > 0.02:
-        frac -= 0.01
-        predicted = lowess(common_ion_rts[:,1], common_ion_rts[:,0], it=50, xvals=common_ion_rts[:,0], frac=frac)
-        new_error = np.sum(common_ion_rts[:,1] - predicted)
-        has_nan = False
-        for value in np.isnan(predicted):
-            has_nan = has_nan or value
-        if not has_nan and abs(new_error) < last_error:
-            full_range = lowess(common_ion_rts[:,1], common_ion_rts[:,0], it=50, xvals=list(range(0,500)), frac=frac)
-            full_range_has_nan = False
-            for value in np.isnan(full_range):
-                full_range_has_nan = full_range_has_nan or value
-            if not full_range_has_nan:
-                last_predicted = predicted
-                last_frac = frac
-
-    if debug:
-        plt.plot(list(range(0,500)), lowess(common_ion_rts[:,1], common_ion_rts[:,0], it=50, xvals=list(range(0,500)), frac=last_frac))    
-        plt.scatter(common_ion_rts[:,0], last_predicted, c='g')
-        plt.scatter(common_ion_rts[:,0], common_ion_rts[:,1], c='k')
-        plt.show()
-        plt.hist(common_ion_rts[:,1] - last_predicted)
-        plt.show()
-
-    t1['pred_t2_rtime'] = lowess(common_ion_rts[:,1], common_ion_rts[:,0], it=50, xvals=t1['rtime'], frac=last_frac)
-    
-    if debug:
-        plt.scatter(t1['rtime'], t1['pred_t2_rtime'])
-        plt.show()
-
-    skip_columns = ['parent_masstrack_id', 'peak_area', 'cSelectivity', 'goodness_fitting', 'snr']
+    skip_columns = ['parent_masstrack_id', 'peak_area', 'cSelectivity', 'goodness_fitting', 'snr', 'detection_counts', 'pred_t2_rtime', 'anchor']
     common_columns = set(list(t1.columns)).intersection(set(list(t2.columns)))
     t1_unique_columns = set([x for x in t1.columns if x not in common_columns])
     t2_unique_columns = set([x for x in t2.columns if x not in common_columns])
 
-    used_t1 = set()
     used_t2 = set()
     new_table = []
+
     t1['rtime_left_base'] = round(t1["rtime_left_base"] - (t1['rtime'] - t1['pred_t2_rtime']), 2)
     t1['rtime_right_base'] = round(t1['rtime_right_base'] - (t1['rtime'] - t1['pred_t2_rtime']), 2)
     t1['rtime'] = round(t1['pred_t2_rtime'], 2)
     t1.drop(labels='pred_t2_rtime', inplace=True, axis=1)
 
+    t1_rows = t1.apply(__extract_row, axis=1, args=([c for c in t1.columns if c not in skip_columns],))
+    t2_rows = t2.apply(__extract_row, axis=1, args=([c for c in t2.columns if c not in skip_columns],))
+    t2_row_map = {x['id_number']: x for x in t2_rows}
 
-    for row_1 in t1.apply(__extract_row, axis=1, args=([c for c in t1.columns if c not in skip_columns],)):
-        mz1 = row_1['mz']
-        for row_2 in t2.apply(__extract_row, axis=1, args=([c for c in t2.columns if c not in skip_columns],)):
-            if mz1 - mz1/1e6 * 5 < row_2['mz'] < mz1 + mz1/1e6 * 5:
-                if row_2['rtime_left_base'] < row_1['rtime'] < row_2['rtime_right_base']:
-                    used_t1.add(row_1['id_number'])
-                    used_t2.add(row_2['id_number'])
-                    new_row = dict(row_1)
-                    new_row.update(row_2)
-                    new_row['detection_counts'] = row_1['detection_counts'] + row_2['detection_counts']
-                    new_table.append(new_row)
+    fluff_1 = {c: 0 for c in t2_unique_columns if c not in skip_columns}
+    fluff_2 = {c: 0 for c in t1_unique_columns if c not in skip_columns}
 
-    for row_1 in t1.apply(__extract_row, axis=1, args=([c for c in t1.columns if c not in skip_columns],)):
-        if row_1['id_number'] not in used_t1:
-            for column in t2_unique_columns:
-                row_1[column] = 0
+    x,y,z = 0,0,0
+    for row_1 in t1_rows:
+        best_match = (np.inf, None)
+        t2_sel = t2[~t2['id_number'].isin(used_t2)]
+        t2_sel = t2_sel[t2_sel['mz'].between(row_1['mz_min'], row_1['mz_max'])]
+        t2_sel = t2_sel[(t2_sel['rtime_left_base'] < row_1['rtime']) & (t2_sel['rtime_right_base'] > row_1['rtime'])]
+        for row_2 in [t2_row_map[t2_id] for t2_id in t2_sel['id_number']]:
+            score = (row_2['mz'] - row_1['mz'])**2 + (row_1['rtime'] - row_2['rtime'])**2
+            if score < best_match[0]:
+                best_match = (score, row_2)
+        if best_match[1]:
+            x += 1
+            row_2 = best_match[1]
+            used_t2.add(row_2['id_number'])
+            new_row = dict(row_1)
+            new_row['id_number'] = 'F' + str(x + y + z)
+            new_row.update(row_2)
+            new_row['rtime_left_base'] = min(row_1['rtime_left_base'], row_2['rtime_left_base'])
+            new_row['rtime_right_base'] = max(row_1['rtime_right_base'], row_2['rtime_right_base'])
+            new_row['rtime'] = (row_1['rtime'] + row_2['rtime'])/2
+            new_table.append(new_row)
+        else:
+            y += 1
+            row_1.update(fluff_1)
+            row_1['id_number'] = 'F' + str(x + y + z)
             new_table.append(row_1)
-
-    for row_2 in t2.apply(__extract_row, axis=1, args=([c for c in t2.columns if c not in skip_columns],)):
+            y += 1
+    for row_2 in t2_rows:
         if row_2['id_number'] not in used_t2:
-            for column in t1_unique_columns:
-                row_2[column] = 0
+            z += 1
+            row_2.update(fluff_2)
+            row_2['id_number'] = 'F' + str(x + y + z)
             new_table.append(row_2)
-        
+    print(x,y,z)
     for i, d in enumerate(new_table):
         d['id_number'] = 'F' + str(i)
-
     new_table = pd.DataFrame(new_table)
-    new_table.sort_values(by=['rtime', 'mz'], inplace=True)
-    new_table.to_csv("new_table2.tsv", sep="\t")
-    return new_table
+    new_table = find_anchors(new_table)
+    return (True, new_table)
 
-if __name__ == '__main__':              
-    t1 = pd.read_csv(sys.argv[1], sep="\t")
-    t2 = pd.read_csv(sys.argv[2], sep="\t")
-    print(join_ftables(t1, t2, dry_run=False))
+
+if __name__ == '__main__':
+    similar_chromatography = True
+    paths = []
+    for dir in os.listdir("."):
+        if "_Pos" in dir and "mzML" in dir:
+            full_path = os.path.join(os.path.abspath("."), dir, "preferred_Feature_table.tsv")
+            paths.append(full_path)
+    mp.freeze_support()
+    join_ftables(paths, similar_chromatography=similar_chromatography)
