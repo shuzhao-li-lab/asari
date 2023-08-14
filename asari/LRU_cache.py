4f36eb122c605c3b71cb0cf98bab10f7d0636d9a
Joshua Mitchell <joshua.mitchell@jax.org>
2023-08-14 15:36:16 -0400

    fix bug in caching

--- asari/LRU_cache.py
+++ asari/LRU_cache.py
@@ -4,13 +4,14 @@
 """
 Memory-aware LRU Cache function decorator
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 A modification of the builtin ``functools.lru_cache`` decorator that takes an
 additional keyword argument, ``use_memory_up_to``. The cache is considered full
 if there are fewer than ``use_memory_up_to`` bytes of memory available.
-If ``use_memory_up_to`` is set, then ``maxsize`` has no effect.
-Uses the ``psutil`` module to get the available memory.
 
-sourced from https://gist.github.com/wmayner/0245b7d9c329e498d42b
+If ``use_memory_up_to`` is set, then ``maxsize`` has no effect.
+
+Uses the ``psutil`` module to get the available memory.
 """
 
 import psutil
@@ -22,6 +23,7 @@ class _HashedSeq(list):
     """ This class guarantees that hash() will be called no more than once
         per element.  This is important because the lru_cache() will hash
         the key multiple times on a cache miss.
+
     """
 
     __slots__ = 'hashvalue'
@@ -38,11 +40,14 @@ def _make_key(args, kwds, typed,
              fasttypes = {int, str, frozenset, type(None)},
              sorted=sorted, tuple=tuple, type=type, len=len):
     """Make a cache key from optionally typed positional and keyword arguments
+
     The key is constructed in a way that is flat as possible rather than
     as a nested structure that would take more memory.
+
     If there is only a single argument and its data type is known to cache
     its hash value, then that argument is returned without a wrapper.  This
     saves space and improves lookup speed.
+
     """
     key = args
     if kwds:
@@ -60,19 +65,26 @@ def _make_key(args, kwds, typed,
 
 def lru_cache(maxsize=128, typed=False, use_memory_up_to=False):
     """Least-recently-used cache decorator.
+
     *use_memory_up_to* is an integer representing the number of bytes of memory
     that must be available on the system in order for a value to be cached. If
     it is set, *maxsize* has no effect.
+
     If *maxsize* is set to None, the LRU features are disabled and the cache
     can grow without bound.
+
     If *typed* is True, arguments of different types will be cached separately.
     For example, f(3.0) and f(3) will be treated as distinct calls with
     distinct results.
+
     Arguments to the cached function must be hashable.
+
     View the cache statistics named tuple (hits, misses, maxsize, currsize)
     with f.cache_info().  Clear the cache and statistics with f.cache_clear().
     Access the underlying function with f.__wrapped__.
+
     See:  http://en.wikipedia.org/wiki/Cache_algorithms#Least_Recently_Used
+
     """
     if use_memory_up_to:
         maxsize=None
@@ -97,6 +109,7 @@ def lru_cache(maxsize=128, typed=False, use_memory_up_to=False):
         root[:] = [root, root, None, None]     # initialize by pointing to self
 
         if use_memory_up_to:
+
             def wrapper(*args, **kwds):
                 # Size limited caching that tracks accesses by recency
                 nonlocal root, hits, misses, full
@@ -113,7 +126,6 @@ def lru_cache(maxsize=128, typed=False, use_memory_up_to=False):
                         link[PREV] = last
                         link[NEXT] = root
                         hits += 1
-                        print(hits / (hits + misses))
                         return result
                 result = user_function(*args, **kwds)
                 with lock:
@@ -149,7 +161,89 @@ def lru_cache(maxsize=128, typed=False, use_memory_up_to=False):
                         last = root[PREV]
                         link = [last, root, key, result]
                         last[NEXT] = root[PREV] = cache[key] = link
-                        full = (psutil.virtual_memory().available < use_memory_up_to)
+                        full = (psutil.virtual_memory().available
+                                < use_memory_up_to)
+                    misses += 1
+                return result
+
+        elif maxsize == 0:
+
+            def wrapper(*args, **kwds):
+                # No caching -- just a statistics update after a successful call
+                nonlocal misses
+                result = user_function(*args, **kwds)
+                misses += 1
+                return result
+
+        elif maxsize is None:
+
+            def wrapper(*args, **kwds):
+                # Simple caching without ordering or size limit
+                nonlocal hits, misses
+                key = make_key(args, kwds, typed)
+                result = cache_get(key, sentinel)
+                if result is not sentinel:
+                    hits += 1
+                    return result
+                result = user_function(*args, **kwds)
+                cache[key] = result
+                misses += 1
+                return result
+
+        else:
+
+            def wrapper(*args, **kwds):
+                # Size limited caching that tracks accesses by recency
+                nonlocal root, hits, misses, full
+                key = make_key(args, kwds, typed)
+                with lock:
+                    link = cache_get(key)
+                    if link is not None:
+                        # Move the link to the front of the circular queue
+                        link_prev, link_next, _key, result = link
+                        link_prev[NEXT] = link_next
+                        link_next[PREV] = link_prev
+                        last = root[PREV]
+                        last[NEXT] = root[PREV] = link
+                        link[PREV] = last
+                        link[NEXT] = root
+                        hits += 1
+                        return result
+                result = user_function(*args, **kwds)
+                with lock:
+                    if key in cache:
+                        # Getting here means that this same key was added to the
+                        # cache while the lock was released.  Since the link
+                        # update is already done, we need only return the
+                        # computed result and update the count of misses.
+                        pass
+                    elif full:
+                        # Use the old root to store the new key and result.
+                        oldroot = root
+                        oldroot[KEY] = key
+                        oldroot[RESULT] = result
+                        # Empty the oldest link and make it the new root.
+                        # Keep a reference to the old key and old result to
+                        # prevent their ref counts from going to zero during the
+                        # update. That will prevent potentially arbitrary object
+                        # clean-up code (i.e. __del__) from running while we're
+                        # still adjusting the links.
+                        root = oldroot[NEXT]
+                        oldkey = root[KEY]
+                        oldresult = root[RESULT]
+                        root[KEY] = root[RESULT] = None
+                        # Now update the cache dictionary.
+                        del cache[oldkey]
+                        # Save the potentially reentrant cache[key] assignment
+                        # for last, after the root and links have been put in
+                        # a consistent state.
+                        cache[key] = oldroot
+                    else:
+                        # Put result in a new link at the front of the queue.
+                        last = root[PREV]
+                        link = [last, root, key, result]
+                        last[NEXT] = root[PREV] = cache[key] = link
+                        full = (len(cache) >= maxsize)
                     misses += 1
                 return result
 
